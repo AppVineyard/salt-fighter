956ed3550d958fc426f7ed55d89fdac52c618136
lastoneawake <honkytonksamurai@gmail.com>
2017-06-22 14:20:40 -0400

    login page update FIRST

--- index.php
+++ index.php
@@ -13,28 +13,26 @@ if(count($_POST)>0) {
 }
 ?>
 <html>
-<head>
-<title>User Login</title>
-<link rel="stylesheet" type="text/css" href="styles.css" />
-</head>
-<body>
-<form name="frmUser" method="post" action="">
-	<div class="message"><?php if($message!="") { echo $message; } ?></div>
-		<table border="0" cellpadding="10" cellspacing="1" width="500" align="center" class="tblLogin">
-			<tr class="tableheader">
-			<td align="center" colspan="2">Enter Login Details</td>
-			</tr>
-			<tr class="tablerow">
-			<td>
-			<input type="text" name="userName" placeholder="User Name" class="login-input"></td>
-			</tr>
-			<tr class="tablerow">
-			<td>
-			<input type="password" name="password" placeholder="Password" class="login-input"></td>
-			</tr>
-			<tr class="tableheader">
-			<td align="center" colspan="2"><input type="submit" name="submit" value="Submit" class="btnSubmit"></td>
-			</tr>
-		</table>
-</form>
-</body></html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <title>ZabSucks : Login</title>
+     <link href="zabSucks.css" type="text/css" rel="stylesheet"/>
+ </head>
+ <body>
+
+
+ <div id="login">
+     <img src="zabSucks.png">
+     <form name="frmUser" method="post" action="">
+         <input type="text" name="userName" placeholder="User Name" class="login-input"><br>
+         <input type="password" name="password" placeholder="Password" class="login-input"><br>
+         <button type="submit" name="submit"> Submit</button>
+     </form>
+
+     <div class="message"></div>
+ </div>
+
+ </body>
+ </html>
+
