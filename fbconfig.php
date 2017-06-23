67e5e48bd2c9c1b06342d23bd959793d3073e885
Andrew Valente <andrewvalente@Andrews-MBP.bluetoad.com>
2017-06-23 17:19:54 -0400

    im breaking everything godspeed

--- fbconfig.php
+++ fbconfig.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/src/Facebook/autoload.php';
 
 $appID      = "1374667435979752";        // APP ID
 $appSecret  = "5cff19ea5fdfe543d8170bc8cc7a7134";    // APP SECRET
-$my_url     = "http://zabsucks.club/";    // APP DOMAIN --> It has to be match with app domain on your facebook app
+$my_url     = "http://www.zabsucks.club/";    // APP DOMAIN --> It has to be match with app domain on your facebook app
  
 
  $fb = new Facebook\Facebook([
