73b90c18257d8acbdcb2c3fdac1d35613ed52808
Andrew Valente <andrewvalente@Andrews-MacBook-Pro.local>
2017-06-23 22:28:22 -0400

    Adding facebook testing directory

--- test/register.php
+++ test/register.php
@@ -18,17 +18,9 @@ $check = mysqli_query($conn,"select * from users where fb_id='$fb_uid'");
 
 $check = mysqli_num_rows($conn,$check);
 
-if (empty($check)) { // if new user . Insert a new record
 
-   $query = "INSERT INTO users (fb_id,fb_first_name,fb_last_name,fb_gender,fb_email,birthday,address) VALUES ('$fb_uid','$fb_firstname','$fb_lastname','$fb_gender','$fb_email',STR_TO_DATE('$birthday', '%m/%d/%Y'),'$address')";
+   $query = "INSERT INTO users (fb_id,fb_first_name,fb_last_name,fb_gender,fb_email,birthday,address) VALUES ('$fb_uid','$fb_firstname','$fb_lastname','$fb_gender','$fb_email',STR_TO_DATE('$birthday', '%m/%d/%Y'),'$address')ON DUPLICATE KEY UPDATE users SET fb_id='$fb_uid', fb_first_name = '$fb_firstname', fb_last_name = '$fb_lastname', fb_gender = '$fb_gender', fb_email='$fb_email', birthday=STR_TO_DATE('$birthday', '%m/%d/%Y'), address='$address' where fb_id='$fb_uid'";
    mysqli_query($conn,$query);
-
-} else {   // If Returned user . update the user record	
-
-   $query = "UPDATE users SET fb_id='$fb_uid', fb_first_name = '$fb_firstname', fb_last_name = '$fb_lastname', fb_gender = '$fb_gender', fb_email='$fb_email', birthday=STR_TO_DATE('$birthday', '%m/%d/%Y'), address='$address' where fb_id='$fb_uid'";
-   mysqli_query($conn,$query);
-
-}
 
 
 
