a5d533e4cc2d1e4ed478e0865eae6e2428e67dd4
lastoneawake <honkytonksamurai@gmail.com>
2017-06-24 20:55:55 -0400

    landing no longer resembles an organic restaurants dumpster at 4pm in mid July in Orlando

--- index.php
+++ index.php
@@ -13,9 +13,81 @@ if(count($_POST)>0) {
 }
 ?>
 <html>
- <html lang="en">
- <script>
-if (document.addEventListener) {
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>ZabSucks</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,200" rel="stylesheet">
+    <link href="index.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+<div id="controller">
+    <div id="shade">
+        <div id="sideBar">
+            <div>
+                <h2 id="topBanner">Login</h2>
+            </div>
+            <div id="login" >
+                <form name="frmUser" method="post" action="">
+                    <input type="text" name="userName" placeholder="User Name" class="login-input"><br>
+                    <input type="password" name="password" placeholder="Password" class="login-input"><br>
+                    <button type="submit" name="submit"> Submit</button>
+                </form>
+                <p>Not a member?</p>
+                <a id="registerBtn">Register</a>
+            </div>
+
+
+            <div id="register">
+                <form name="regUser" method="post" action="">
+                    <input type="text" name="fName" placeholder="First Name" class="register-input"><br>
+                    <input type="text" name="lName" placeholder="Last Name" class="register-input"><br>
+                    <input type="text" name="userName" placeholder="User Name" class="login-input"><br>
+                    <input type="password" name="password" placeholder="Password" class="login-input"><br>
+                    <button type="submit" name="submit"> Submit</button>
+                </form>
+                <p>Already a member?</p>
+                <a id="loginBtn">Login</a>
+            </div>
+        </div>
+        <div id="whatUp">
+            <img class="zabSucksImg" src="zabSucks.png" />
+
+            <p>fixie brooklyn. Bushwick cray migas fanny pack pok pok. Church-key brooklyn pabst, chambray cloud bread aesthetic green juice air plant hashtag cornhole try-hard activated charcoal. 90's bitters pinterest etsy, hell of succulents
+                <br>Thundercats williamsburg DIY, polaroid ugh XOXO try-hard chia kitsch hell of mustache. Cronut helvetica artisan whatever irony bespoke lyft narwhal humblebrag banjo actually. Tbh sriracha mixtape sartorial flexitarian tumblr ennui freegan cray +1 raclette air plant. 90's pok pok wayfarers cornhole.
+                <br>
+                <br>Tattooed shabby chic church-key, air plant mlkshk etsy brooklyn shaman direct trade glossier biodiesel. Messenger bag yuccie polaroid tumeric tofu letterpress aesthetic hoodie
+
+
+            </p>
+        </div>
+    </div>
+</div>
+
+<script>
+    var loginBtn = document.getElementById('loginBtn');
+    var regBtn = document.getElementById('registerBtn');
+    var login = document.getElementById('login');
+    var register = document.getElementById('register');
+    var topB = document.getElementById('topBanner');
+
+    regBtn.addEventListener('click', function(){
+        login.style.display = 'none';
+        register.style.display = 'block';
+        topB.innerHTML = 'Register';
+
+        console.log(topB);
+    });
+    loginBtn.addEventListener('click', function(){
+        register.style.display = 'none';
+        login.style.display = 'block';
+        topB.innerHTML = 'Login';
+
+    });
+
+    if (document.addEventListener) {
         document.addEventListener('contextmenu', function(e) {
             alert("Zab, stop trying to steal our code."); //here you draw your own menu
             e.preventDefault();
@@ -27,29 +99,6 @@ if (document.addEventListener) {
         });
     }
 </script>
- <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1">
-     <title>ZabSucks : Login</title>
-     <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
-     <link href="zabSucks.css" type="text/css" rel="stylesheet"/>
- </head>
- <body>
-
-
- <div id="login">
-     <img src="zabSucks.png">
-     <form name="frmUser" method="post" action="">
-         <input type="text" name="userName" placeholder="User Name" class="login-input"><br>
-         <input type="password" name="password" placeholder="Password" class="login-input"><br>
-         <button type="submit" name="submit"> Submit</button>
-     </form>
-
-     <div class="message"><?php if($message!="") { echo $message; } ?></div>
-     <div class="bioImg"><?php if($row["bio_url"] !="") { echo '<img src="'.$row["bio_url"].'">'; } ?>
- </div> 
-</div>
-
- </body>
- </html>
+</body>
+</html>
 
