9e09441866885eded5eea9ca457e5f6cb2e51cfb
Andrew Valente <andrewvalente@Andrews-MacBook-Pro.local>
2017-06-27 00:02:53 -0400

    making test page for inputting match data

---
+++ create_match.php
@@ -0,0 +1,54 @@
+<?php
+//session_start();
+//
+//$fb_uid = $_SESSION['fb_id'] ;           
+//$fb_firstname = $_SESSION['firstname'];
+//$fb_lastname = $_SESSION['lastname'];
+//$fb_gender = $_SESSION['gender'];
+//$fb_email = $_SESSION['email'];
+//
+//$birthday = $_POST['birthday'];
+//$address = $_POST['address'];
+//$match_date = $_POST['match_date'];
+$match_id = $_POST['match_id'];
+$win = $_POST['win'];
+$loss = $_POST['loss'];
+$p1_id = $_POST['p1_id'];
+$p2_id = $_POST['p2_id'];
+$p1_char = $_POST['p1_char'];
+$p2_char = $_POST['p2_char'];
+    
+
+require 'dbconfig.php';
+
+
+$check = mysqli_query($conn,"select * from match_stats where match_id='$match_id'");
+
+$check = mysqli_num_rows($check);
+
+if (empty($check)) { // if new user . Insert a new record
+
+   $query = "INSERT INTO match_stats (match_date,match_id,win,loss,p1_id,_p2_id,p1_char,p2_char) VALUES (UNIX_TIMESTAMP(),'$match_id','$win','$loss','$p1_id','$p2_id','$p1_char','$p2_char')";
+   mysqli_query($conn,$query);
+
+} else {   // If Returned user . update the user record	
+
+   $query = "UPDATE match_stats SET firstname= '$firstname', lastname = '$lastname', password = '$password' where userName='$userName'";
+   mysqli_query($conn,$query);
+    
+}
+
+
+
+//if (!mysql_query($query,$conn)){
+//    die('Error: ' . mysql_error());
+//}
+
+echo "<h2>Congratulation, you have been registered</h2>";
+echo  "<br>";
+echo  "redirecting...";
+
+header('Refresh: 3; URL=home.html');
+ 
+mysql_close($conn);
+?>
---
+++ register_match.php
@@ -0,0 +1,102 @@
+<?php
+
+    require 'dbconfig.php';
+    
+    
+//    $message="";
+//if(count($_POST)>0) {
+//	$conn = mysqli_connect("us-cdbr-iron-east-03.cleardb.net","bee6bfe3a31317","789e80c3","heroku_2154a2bf255ffd7");
+//	$result = mysqli_query($conn,"SELECT * FROM users WHERE userName='" . $_POST["userName"] . "' and password = '". $_POST["password"]."'");
+//	$count  = mysqli_num_rows($result);
+//	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+//	if($count==0) {
+//		$message = "Invalid Username or Password!";
+//	} else {
+//		$message = 'Welcome '.$row["displayName"].', '.'You are successfully authenticated!'."</br>".$row["qotd"];
+//	}
+//}
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>ZabSucks</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,200" rel="stylesheet">
+    <link href="index.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+<div id="shade"><div id="sideBar"><h2 id="topBanner">Login</h2>
+
+        <div id="login">
+            <form name="frmUser" method="post" action="">
+                <input type="text" name="userName" placeholder="User Name" class="login-input"><br>
+                <input type="password" name="password" placeholder="Password" class="login-input"><br>
+                <button type="submit" name="submit" value="Register"> Submit</button>
+            </form>
+            <p>Not a member?</p>
+            <a id="registerBtn">Register</a>
+        </div>
+
+
+        <div id="register">
+            <form name="regUser" method="post" action="create_match.php">
+                <input type="text" name="match_id" placeholder="Match ID" class="register-input"><br>
+                <input type="text" name="win" placeholder="Win" class="register-input"><br>
+                <input type="text" name="loss" placeholder="loss" class="register-input"><br>
+                <input type="text" name="p1_id" placeholder="Player 1 ID" class="register-input"><br>
+                <input type="text" name="p2_id" placeholder="Player 2 ID" class="register-input"><br>
+                <input type="text" name="p1_char" placeholder="Player 1 Character" class="register-input"><br>
+                <input type="text" name="p2_char" placeholder="Player 2 Character" class="register-input"><br>
+                <button type="submit" name="submit" value=""> Submit</button>
+            </form>
+            <p>Already a member?</p>
+            <a id="loginBtn">Login</a>
+        </div>
+    </div><!--
+    -->
+    <div id="whatUp">
+        <img class="zabSucksImg" src="images/zabSucks.png"/>
+
+        <p>fixie brooklyn. Bushwick cray migas fanny pack pok pok. Church-key brooklyn pabst, chambray cloud bread
+            aesthetic green juice air plant hashtag cornhole try-hard activated charcoal. 90's bitters pinterest etsy,
+            hell of succulents
+            <br>Thundercats williamsburg DIY, polaroid ugh XOXO try-hard chia kitsch hell of mustache. Cronut helvetica
+            artisan whatever irony bespoke lyft narwhal humblebrag banjo actually. Tbh sriracha mixtape sartorial
+            flexitarian tumblr ennui freegan cray +1 raclette air plant. 90's pok pok wayfarers cornhole.
+            <br>
+            <br>Tattooed shabby chic church-key, air plant mlkshk etsy brooklyn shaman direct trade glossier biodiesel.
+            Messenger bag yuccie polaroid tumeric tofu letterpress aesthetic hoodie
+
+
+        </p>
+    </div>
+</div>
+
+
+<script>
+    var loginBtn = document.getElementById('loginBtn');
+    var regBtn = document.getElementById('registerBtn');
+    var login = document.getElementById('login');
+    var register = document.getElementById('register');
+    var topB = document.getElementById('topBanner');
+
+    regBtn.addEventListener('click', function () {
+        login.style.display = 'none';
+        register.style.display = 'block';
+        topB.innerHTML = 'Register';
+
+        console.log(topB);
+    });
+    loginBtn.addEventListener('click', function () {
+        register.style.display = 'none';
+        login.style.display = 'block';
+        topB.innerHTML = 'Login';
+
+    });
+
+
+</script>
+</body>
+</html>
+
