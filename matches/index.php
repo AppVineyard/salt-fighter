a478bfe66d2cba314958ec400d91123a73687b8a
Andrew Valente <andrewvalente@Andrews-MBP.bluetoad.com>
2017-06-28 14:30:54 -0400

    added date range form

--- matches/index.php
+++ matches/index.php
@@ -62,7 +62,7 @@
         <tr>
 			<td><?php echo $row["p1_char"]; ?></td>
 			<td><?php echo $row["p2_char"]; ?></td>
-			<td><?php echo $row["match_date"]?></td>
+            <td><?php echo $row["match_date"];?></td>
 
 		</tr>
    <?php
