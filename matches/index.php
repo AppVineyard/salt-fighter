c9e82e7eda0d69e88eaa462abdba7e2812292165
Andrew Valente <andrewvalente@andrew-macbook.bluetoad.com>
2017-06-28 14:45:49 -0400

    added date range form

--- matches/index.php
+++ matches/index.php
@@ -62,7 +62,7 @@
         <tr>
 			<td><?php echo $row["p1_char"]; ?></td>
 			<td><?php echo $row["p2_char"]; ?></td>
-			<td><?php echo date("F j, Y",$row["match_date"]); ?></td>
+			<td><?php echo date("F j, Y g:i a",$row["match_date"]); ?></td>
 
 		</tr>
    <?php
