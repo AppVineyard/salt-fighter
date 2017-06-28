13332ac5705ce41e08e7eb8dbee8b5f8e9123b60
Andrew Valente <andrewvalente@andrew-macbook.bluetoad.com>
2017-06-28 15:24:33 -0400

    added date range form

--- matches/index.php
+++ matches/index.php
@@ -80,7 +80,7 @@ showOn: "button",
 buttonImage: "datepicker.png",
 buttonText: "Date Picker",
 buttonImageOnly: true,
-dateFormat: 'mm-dd-yy'  
+dateFormat: 'dd-mm-yy'  
 });
 $(function() {
 $("#match_date").datepicker();
