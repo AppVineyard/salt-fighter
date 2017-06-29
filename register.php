cc157ae05412f758503f189ab7d46b8a9f1a036d
Andrew Valente <andrewvalente@Andrews-MacBook-Pro.local>
2017-06-29 00:35:25 -0400

    added login.php and sessions stuff

--- register.php
+++ register.php
@@ -47,7 +47,7 @@ echo "<h2>Congratulation, you have been registered</h2>";
 echo  "<br>";
 echo  "redirecting...";
 
-header('Refresh: 3; URL=home.html');
+header('Refresh: 3; URL=register_match.php');
  
 mysql_close($conn);
 ?>
