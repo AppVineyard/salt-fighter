5a99c22db99b3fdea53eb4a2b1403855a3a7bfdd
AppVineyard <andrewvalentedev@gmail.com>
2017-06-29 23:10:31 -0400

    Removed required title
    Removed required title

--- index.php
+++ index.php
@@ -41,9 +41,9 @@
 
         <div id="register">
             <form name="regUser" method="post" action="register.php">
-                <input type="text" required="required" required title="Please fill out all fields" name="firstname" placeholder="First Name" class="register-input"><br>
-                <input type="text" required="required" required title="Please fill out all fields"name="lastname" placeholder="Last Name" class="register-input"><br>
-                <input type="text" required="required" required title="Please fill out all fields"name="userName" placeholder="User Name" class="register-input"><br>
+                <input type="text" required="required" name="firstname" placeholder="First Name" class="register-input"><br>
+                <input type="text" required="required" name="lastname" placeholder="Last Name" class="register-input"><br>
+                <input type="text" required="required" name="userName" placeholder="User Name" class="register-input"><br>
                 <input type="password" required="required" required title="Please fill out all fields"name="password" placeholder="Password" class="register-input"><br>
                 <button type="submit" name="submit" value=""> Submit</button>
             </form>
