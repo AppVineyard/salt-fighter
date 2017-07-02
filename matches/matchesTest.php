b9f2effb02474ea3cfde0249ff6de68b084005aa
htsamurai <honkytonksamurai@gmail.com>
2017-07-02 17:07:50 -0400

    tests, a lot will be called that

--- matches/matchesTest.php
+++ matches/matchesTest.php
@@ -42,9 +42,9 @@ $result = mysqli_query($conn, $sql);
 <title>Salt-Figher : Match History</title>
 <script src="https://code.jquery.com/jquery-1.9.1.js"></script>
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-
-<link rel="stylesheet" type="text/css" href="matchesTest.css">
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="matchesTest.css">
+
 </head>
 
 <body>
