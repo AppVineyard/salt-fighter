47e0c48254e3c40882690938da0b606e96e71371
AppVineyard <andrewvalentedev@gmail.com>
2017-07-02 03:21:31 -0400

    Add Konami code to trigger salty fighter

--- index.php
+++ index.php
@@ -94,6 +94,18 @@
 
 
 </script>
+
+<script type="text/javascript">
+if ( window.addEventListener ) {  
+  var state = 0, konami = [38,38,40,40,37,39,37,39,66,65];  
+  window.addEventListener("keydown", function(e) {  
+    if ( e.keyCode == konami[state] ) state++;  
+    else state = 0;  
+    if ( state == 10 )  
+      window.location = "https://salt-fighter.herokuapp.com/SaltyFighter/index.html";  //you can write your own code here
+    }, true);  
+}  
+</script>
 </body>
 </html>
 
