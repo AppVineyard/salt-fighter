275a766da9407267e9e1b1a65b01e2d00ba061af
Andrew Valente <andrewvalente@andrew-macbook.bluetoad.com>
2017-07-03 12:03:19 -0400

    added display score and add score files

--- addscore.php
+++ addscore.php
@@ -18,18 +18,18 @@ require 'dbconfig.php';
 
         
         // Strings must be escaped to prevent SQL injection attack.
-        $name = mysqli_real_escape_string($conn,"Winner");
-        $score = mysqli_real_escape_string($conn,"100");
+        $name = mysqli_real_escape_string($conn,$_GET['name']);
+        $score = mysqli_real_escape_string($conn,$_GET['score']);
         $hash = $_GET['hash'];
         
         $secretKey="salty"; # Change this value to match the value stored in the client javascript below
         
         $real_hash = md5($name . $score . $secretKey);
-        //if($real_hash == $hash) {
-            // Send variables for the MySQL database class.
+        if($real_hash == $hash) {
+             Send variables for the MySQL database class.
             $query = "insert into scores values (NULL, '$name', '$score');";
             $result = mysqli_query($conn,$query) or die('Query failed: ' . mysqli_error());
-        //}
+        }
 
     
     
