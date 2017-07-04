61abe47f65f3cf3056ba5cd945db36106435ce28
AppVineyard <andrewvalentedev@gmail.com>
2017-07-04 11:18:39 -0400

    testing

---
+++ SaltyFighter/addscore.php
@@ -0,0 +1,51 @@
+<?php
+//session_start();
+//
+//$fb_uid = $_SESSION['fb_id'] ;           
+//$fb_firstname = $_SESSION['firstname'];
+//$fb_lastname = $_SESSION['lastname'];
+//$fb_gender = $_SESSION['gender'];
+//$fb_email = $_SESSION['email'];
+//
+//$birthday = $_POST['birthday'];
+//$address = $_POST['address'];
+//$userName = $_POST['userName'];
+//$firstname = $_POST['firstname'];
+//$lastname = $_POST['lastname'];
+//$password = $_POST['password'];
+
+require 'dbconfig.php';
+
+        
+        // Strings must be escaped to prevent SQL injection attack.
+        $name = mysqli_real_escape_string($conn,$_GET['name']);
+        $score = mysqli_real_escape_string($conn,$_GET['score']);
+        $hash = $_GET['hash'];
+        
+        $secretKey="salty"; # Change this value to match the value stored in the client javascript below
+        
+        $real_hash = md5($name . $score . $secretKey);
+        if($real_hash == $hash) {
+             Send variables for the MySQL database class.
+            $query = "insert into scores values (NULL, '$name', '$score');";
+            $result = mysqli_query($conn,$query) or die('Query failed: ' . mysqli_error());
+        }
+
+    
+    
+
+
+
+
+//if (!mysql_query($query,$conn)){
+//    die('Error: ' . mysql_error());
+//}
+
+//echo "<h2>Congratulation, you have been registered</h2>";
+//echo  "<br>";
+//echo  "redirecting...";
+//
+//header('Refresh: 3; URL=home.html');
+ 
+//mysql_close($conn);
+?>
---
+++ SaltyFighter/display.php
@@ -0,0 +1,49 @@
+<?php
+//session_start();
+//
+//$fb_uid = $_SESSION['fb_id'] ;           
+//$fb_firstname = $_SESSION['firstname'];
+//$fb_lastname = $_SESSION['lastname'];
+//$fb_gender = $_SESSION['gender'];
+//$fb_email = $_SESSION['email'];
+//
+//$birthday = $_POST['birthday'];
+//$address = $_POST['address'];
+//$userName = $_POST['userName'];
+//$firstname = $_POST['firstname'];
+//$lastname = $_POST['lastname'];
+//$password = $_POST['password'];
+
+require 'dbconfig.php';
+
+    
+
+    // Send variables for the MySQL database class.
+    
+    $query = "SELECT * FROM `scores` ORDER by `score` DESC LIMIT 5";
+    $result = mysqli_query($conn,$query) or die('Query failed: ' . mysqli_error());
+    
+    $num_results = mysqli_num_rows($result);
+    
+    for($i = 0; $i < $num_results; $i++)
+    {
+        $row = mysqli_fetch_array($result);
+        echo $row['name'] . "\t" . $row['score'] . "\n";
+    }
+    
+
+
+
+
+//if (!mysql_query($query,$conn)){
+//    die('Error: ' . mysql_error());
+//}
+
+//echo "<h2>Congratulation, you have been registered</h2>";
+//echo  "<br>";
+//echo  "redirecting...";
+//
+//header('Refresh: 3; URL=home.html');
+ 
+//mysql_close($conn);
+?>
