37b8964d7ec69e0a3b597a62285b9b947c494878
lastoneawake <honkytonksamurai@gmail.com>
2017-07-08 12:48:16 -0400

    should be actually responsive now

--- matches/matchesTest.php
+++ matches/matchesTest.php
@@ -41,6 +41,7 @@ $result = mysqli_query($conn, $sql);
 <html>
 <title>Salt-Figher : Match History</title>
 <script src="https://code.jquery.com/jquery-1.9.1.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
