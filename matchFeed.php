8e685aefa882c0be3603ba34a3c9f673bb906dab
Andrew Valente <andrewvalente@andrew-macbook.bluetoad.com>
2017-08-22 16:39:51 -0400

    Added First Json Feed

--- matchFeed.php
+++ matchFeed.php
@@ -2,10 +2,24 @@
     
     require 'dbconfig.php';
    
-    $sql = "SELECT match_date, match_id, win, loss, p1_id, p2_id, p1_char, p2_char FROM match_stats INNER JOIN users ON match_stats.win = users.player_id ORDER BY match_date desc";
+    $sql = "SELECT match_date, match_date, win, loss, p1_id, p2_id, p1_char, p2_char, users.userName FROM match_stats INNER JOIN users ON match_stats.win = users.player_id ORDER BY match_date desc";
     $result2 = mysqli_query($conn, $sql);
+
+    $data = array();
     
-    $row = mysqli_fetch_array($result2);
-    echo json_encode($row);
+    while ($row = mysqli_fetch_array($result2)) {
+        $data = array(
+        $row['userName'],
+        $row['match_date'],
+        $row['match_date'],
+        $row['win'],
+        $row['loss'],
+        $row['p1_id'],
+        $row['p2_id'],
+        $row['p1_char'],
+        $row['p2_char']);
+    }
+
+    echo json_encode($data);
 
 ?>
